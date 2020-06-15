6acdec1051ffc6f3a95ee2fc8430f0d3ce35da7c
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-15 18:15:04 -0300

    Operators

---
+++ introduction/comparison-operators/index.js
@@ -0,0 +1,13 @@
+/* ========================================================
+    Comparison operators
+
+    > Maior
+    < Menor
+    >= Maior ou igual
+    >= Menor ou igual
+    == Igual
+    === Igual e do mesmo tipo
+    != Diferente
+    !== Diferente, inclusive tipo
+
+==========================================================*/
