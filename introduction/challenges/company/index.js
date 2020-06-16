8024ce282d923d50e85afad6675256c01edfb430
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-16 17:42:53 -0300

    Finish company challenge

---
+++ introduction/challenges/company/index.js
@@ -0,0 +1,10 @@
+const company = {
+    name: 'Rocketseat',
+    color: 'Purple',
+    address: {
+        street: 'Rua Guilherme Gembala',
+        number: 260
+    }
+}
+
+console.log(`A empresa ${company.name} está localizada em ${company.address.street}, ${company.address.number}`);
