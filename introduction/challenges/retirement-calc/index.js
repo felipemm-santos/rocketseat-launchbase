e3ef40056909258a99dc1578da1802f3a87df4f1
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-16 17:27:02 -0300

    Add another way of resolve challenge

--- introduction/challenges/retirement-calc/index.js
+++ introduction/challenges/retirement-calc/index.js
@@ -1,22 +1,53 @@
+// const name = 'Felipe';
+// const gender = 'M';
+// const age = 58;
+// const contributionTime = 37;
+
+// const agePlusContributionTime = age + contributionTime;
+
+// if (gender == 'F') {
+//   var minContributionTime = 30;
+//   var minAgePlusContributionTime = 85;
+// } else {
+//   var minContributionTime = 35;
+//   var minAgePlusContributionTime = 95;
+// }
+
+// if (
+//   contributionTime >= minContributionTime &&
+//   agePlusContributionTime >= minAgePlusContributionTime
+// ) {
+//   console.log(`${name}, você pode se aposentar`);
+// } else {
+//   console.log(`${name}, você ainda não pode se aposentar`);
+// }
+
+// Another way
+
 const name = 'Felipe';
 const gender = 'M';
 const age = 58;
 const contributionTime = 37;
 
-const agePlusContributionTime = age + contributionTime;
+const retirementCalc = age + contributionTime;
 
-if (gender == 'F') {
-  var minContributionTime = 30;
-  var minAgePlusContributionTime = 85;
-} else {
-  var minContributionTime = 35;
-  var minAgePlusContributionTime = 95;
-}
+const minContributionTimeMan = 35;
+const minContributionTimeWoman = 30;
 
-if (
-  contributionTime >= minContributionTime &&
-  agePlusContributionTime >= minAgePlusContributionTime
-) {
+const minRetirementCalcMan = 95;
+const minRetirementCalcWoman = 85;
+
+const manCanRitire =
+  gender == 'M' &&
+  contributionTime >= minContributionTimeMan &&
+  retirementCalc >= minRetirementCalcMan;
+
+const womanCanRitire =
+  gender == 'F' &&
+  contributionTime >= minContributionTimeMan &&
+  retirementCalc >= minRetirementCalcWoman;
+
+if (manCanRitire || womanCanRitire) {
   console.log(`${name}, você pode se aposentar`);
 } else {
   console.log(`${name}, você ainda não pode se aposentar`);
