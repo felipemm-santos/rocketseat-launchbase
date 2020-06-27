c354a06d702590bb9d42f156086e4cf35005f4f6
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-27 12:51:44 -0300

    Maximize modl challenge finished

--- front-end/challenges/c3-content/scripts/script.js
+++ front-end/challenges/c3-content/scripts/script.js
@@ -1,6 +1,14 @@
-const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
 
+const modalOverlay = document.querySelector('.modal-overlay');
+
+const modal = document.querySelector('.modal');
+
+const maximizeModal = document.querySelector('.maximize-modal');
+
+const maximizeModalExit = document.querySelector('.maximize-modal-exit');
+
+// Open modal
 for (const card of cards) {
   card.addEventListener('click', () => {
     const cardId = card.id;
@@ -11,6 +19,27 @@ for (const card of cards) {
   });
 }
 
-document
-  .querySelector('.close-modal')
-  .addEventListener('click', () => modalOverlay.classList.remove('active'));
+// Close modal
+
+document.querySelector('.close-modal').addEventListener('click', () => {
+  modalOverlay.classList.remove('active');
+  modalOverlay.querySelector('iframe').src = '';
+  modal.classList.remove('maximize');
+  maximizeModal.classList.remove('hidden');
+  maximizeModalExit.classList.add('hidden');
+});
+
+// Maximize mdoal
+
+maximizeModal.addEventListener('click', () => {
+  modal.classList.add('maximize');
+  maximizeModal.classList.add('hidden');
+  maximizeModalExit.classList.remove('hidden');
+});
+
+// Maximize mdoal exit
+maximizeModalExit.addEventListener('click', () => {
+  modal.classList.remove('maximize');
+  maximizeModalExit.classList.add('hidden');
+  maximizeModal.classList.remove('hidden');
+});
